0fa26572890b507edafb382799b0e12ff80c62cf
Farabi Hussain <farabihussain8@gmail.com>
2024-08-08 19:07:43 -0500

    fixed receipt searching only lowercase

--- Popups.py
+++ Popups.py
@@ -2,17 +2,17 @@ from CTkMessagebox import CTkMessagebox
 
 class InfoPopup():
     def __init__(self, msg="InfoPopup") -> None:
-        CTkMessagebox(title="Info", message=f"\n{msg}\n", height=250)
+        CTkMessagebox(title="Info", message=f"\n{msg}\n")
 
 
 class ErrorPopup():
     def __init__(self, msg="ErrorPopup") -> None:
-        CTkMessagebox(title="Error", message=f"\n{msg}\n", icon="cancel", height=250)
+        CTkMessagebox(title="Error", message=f"\n{msg}\n", icon="cancel")
 
 
 class PromptPopup():
     def __init__(self, msg="PromptPopup", func=lambda:()) -> None:
-        self.prompt = CTkMessagebox(title="Confirm", message=f"\n{msg}\n", icon="question", option_1="Yes", option_2="Cancel", height=250)
+        self.prompt = CTkMessagebox(title="Confirm", message=f"\n{msg}\n", icon="question", option_1="Yes", option_2="Cancel")
         self.func = func
 
         if (self.prompt.get() == "Yes"):
